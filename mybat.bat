433abb9bc956ac67dd1d3c84e0a4674fb8193182
satyasuresh1 <sureshconnel@gmail.com>
2020-10-02 15:52:53 -0700

    batman

--- mybat.bat
+++ mybat.bat
@@ -7,7 +7,4 @@ REM SS UU RR EE SS HH           //
 REM SS UU RR EE SS HH           //
 REM///////////////////////////////
 git pull
-git echo "i am here to create avengers file" >> ironman.go
-git add -A
-git commit -m "ironman avengers file add chesadu"
 git push
