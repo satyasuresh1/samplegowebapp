af867696838096ca1723d963497e71891f7376a9
satyasuresh1 <sureshconnel@gmail.com>
2020-10-02 15:50:26 -0700

    ironman avengers file add chesadu

--- mybat.bat
+++ mybat.bat
@@ -1,6 +1,13 @@
+REM///////////////////////////////
+REM SS UU RR EE SS HH           //
+REM SS UU RR EE SS HH           //
+REM SS UU RR EE SS HH           //
+REM SS UU RR EE SS HH           //
+REM SS UU RR EE SS HH           //
+REM SS UU RR EE SS HH           //
+REM///////////////////////////////
 git pull
-git echo "i am here to create avengers file" >> avengers.java
+git echo "i am here to create avengers file" >> ironman.go
 git add -A
-git commit -m "ganesh gadu avengers file add chesadu"
+git commit -m "ironman avengers file add chesadu"
 git push
-color03
